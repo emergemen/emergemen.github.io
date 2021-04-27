37a2bfe0ee8a0e6bf676dbefb91b9f91d90a07ff
Scott Isaacs <scott@capewoodplace.com>
2021-04-26 23:06:04 -0700

    wip

--- service-worker.js
+++ service-worker.js
@@ -15,6 +15,7 @@ var urlsToCache = [
 	"/css/bootstrap-reboot.css",
 	"/css/bootstrap-reboot.min.css"
 ];
+
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -24,4 +25,30 @@ self.addEventListener('install', function(event) {
   );
 });
 
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+    .then(function(response) {
+      return response || fetchAndCache(event.request);
+    })
+  );
+});
 
+function fetchAndCache(url) {
+  return fetch(url)
+  .then(function(response) {
+    // Check if we received a valid response
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return caches.open(CACHE_NAME)
+    .then(function(cache) {
+      cache.put(url, response.clone());
+      return response;
+    });
+  })
+  .catch(function(error) {
+    console.log('Request failed:', error);
+    // You could return a custom offline 404 page here
+  });
+}
